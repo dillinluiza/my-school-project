bc9a4c99c015318352de75ee88d0893410b61cb5
dillinluiza <dillinluiza@gmail.com>
2025-03-13 10:28:57 +0000

    Add 8.c

---
+++ src/8.c
@@ -0,0 +1,10 @@
+
+int main(void) {
+    int x = 5;
+    if (x < 10) {
+        printf("x is less than 10\n");
+    } else {
+        printf("x is greater than or equal to 10\n");
+    }
+    return 0;
+}
