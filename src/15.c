e998dcc3cb385a073c342ca7036767b1aa69bf52
dillinluiza <dillinluiza@gmail.com>
2025-03-20 10:38:37 +0000

    Add 15.c

---
+++ src/15.c
@@ -0,0 +1,12 @@
+
+  #include <stdio.h>
+
+int main() {
+    int x = 5;
+    if (x == 5) {
+        printf("The number is five.\n");
+    } else {
+        printf("The number is not five.\n");
+    }
+    return 0;
+}
